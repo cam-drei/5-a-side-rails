08eb11445856c6e07f84c0a2748b76577b476e7a
Dung Luu <dung.luu07@gmail.com>
2019-12-23 14:57:44 +0700

    edit controller show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    articles_in_category = Article.joins(:article_categories).where('category_id = ?', params[:id])
+    params[:id]
+    articles_in_category = Article.joins(:article_categories).where(article_categories: {category_id: params[:id]})
     render json: articles_in_category.to_json
   end
 
